e1e626e06b4a28bbf584a4455f00323b8be854eb
nlewis84 <nathan@differential.com>
2022-01-14 10:34:48 -0600

    chore: setup Amplitude

--- src/Providers.js
+++ src/Providers.js
@@ -9,8 +9,13 @@ import {
   ACCEPT_FOLLOW_REQUEST,
 } from '@apollosproject/ui-connected';
 import { checkOnboardingStatusAndNavigate } from '@apollosproject/ui-onboarding';
+// import ApollosConfig from '@apollosproject/config';
+import { Amplitude } from '@amplitude/react-native';
 
 import ClientProvider, { client } from './client';
+
+const amplitude = Amplitude.getInstance();
+amplitude.init('a8690cd47b92611a0d0b60f3598ee14a');
 
 const AppProviders = ({ children }) => (
   <ClientProvider>
@@ -52,7 +57,12 @@ const AppProviders = ({ children }) => (
           })
         }
       >
-        <AnalyticsProvider>
+        <AnalyticsProvider
+          trackFunctions={[
+            ({ eventName, properties }) =>
+              amplitude.logEvent(eventName, properties),
+          ]}
+        >
           <LiveProvider>{children}</LiveProvider>
         </AnalyticsProvider>
       </AuthProvider>
